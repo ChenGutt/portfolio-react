17fb3e9694f7640644990719d8e8afa68e71819e
ChenGutt <chen.gutt1@gmail.com>
2021-06-13 10:56:55 +0200

    youHaveToWorkNow

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -153,12 +153,9 @@ function Portfolio(props){
                 <div className="img-box-wrapper row justify-content-center">
     
                     <div className="col-md-6 col-lg-4 portfolio-img-box" onClick={openPopupBoxIstay}>
-                       
-                            <img src={iStay} alt="istay angular project" className="portfolio-img" />
-                            <div className="overflow"></div>
-                            <FontAwesomeIcon icon = {faSearchPlus} className="portfolio-icon"/>
-                            <button className="btn btn-info">ddd</button>
-                        
+                        <img src={iStay} alt="istay angular project" className="portfolio-img" />
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon icon = {faSearchPlus} className="portfolio-icon"/>
                     </div>
                 
              
